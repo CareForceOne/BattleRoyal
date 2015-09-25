908701b355c95f6c786a8e00d3168b8e3dd1943e
unknown <mlviscon@portal.lab.huskygames.com>
2015-09-25 18:28:32 -0400

    Added networking and fixed movement

--- Battle Royal/Assets/Scripts/Player_Movement.cs
+++ Battle Royal/Assets/Scripts/Player_Movement.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class Player_Movement : MonoBehaviour {
 
-	float speed = 5.00f;
+	public float speed = 5.00f;
 	float buttonTimer = 0f;
 	float DASHTIME = 0f;
+	float dashCooldownTimer = 0.0f;
+	public float DASHCOOLDOWN = 3.0f;
+    public float JUMPCOOLDOWN = 0.5f;
+    float jumpTimer = 0.0f; 
 	// Use this for initialization
 	void Start () {
 		
@@ -13,33 +17,47 @@ public class Player_Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(buttonTimer > 0){
-			buttonTimer -= Time.deltaTime; 
-		}
+        if (!GetComponent<NetworkView>().isMine)
+        {
+            if (buttonTimer > 0)
+            {
+                buttonTimer -= Time.deltaTime;
+            }
 
-		if (DASHTIME > 0) {
-			DASHTIME -= Time.deltaTime;
-		}
-		if (Input.GetButtonDown ("Horizontal") && buttonTimer > 0) {
-			//double dash code
-			speed = 50.0f;
-			DASHTIME = 0.3f;
-			//Debug.Log("DASH");
-		}
-		if (DASHTIME <= 0) {
-			speed = 5.00f;
-		}
+            if (DASHTIME > 0)
+            {
+                DASHTIME -= Time.deltaTime;
+            }
+            if (dashCooldownTimer > 0)
+            {
+                dashCooldownTimer -= Time.deltaTime;
+            }
+            if (Input.GetButtonDown("Horizontal") && buttonTimer > 0 && dashCooldownTimer <= 0)
+            {
+                //double dash code
+                speed = 50.0f;
+                DASHTIME = 0.3f;
+                dashCooldownTimer = DASHCOOLDOWN;
+                //Debug.Log("DASH");
+            }
+            if (DASHTIME <= 0)
+            {
+                speed = 5.00f;
+            }
 
-		if (Input.GetButtonDown("Horizontal")){
-			buttonTimer = 0.5f;
-		}
+            if (Input.GetButtonDown("Horizontal"))
+            {
+                buttonTimer = 0.5f;
+            }
 
-		if (Input.GetButton("Horizontal")) {
-			//transform.Translate((Input.GetAxis("Horizontal") * Vector3.right) * Time.deltaTime * speed);
+            if (Input.GetButton("Horizontal"))
+            {
+                //transform.Translate((Input.GetAxis("Horizontal") * Vector3.right) * Time.deltaTime * speed);
 
-			GetComponent<Rigidbody2D>().velocity = new Vector2(speed * Input.GetAxis("Horizontal"), GetComponent<Rigidbody2D>().velocity.y);
-			//Debug.Log (GetComponent<Rigidbody2D>().velocity);
-		}
+                GetComponent<Rigidbody2D>().velocity = new Vector2(speed * Input.GetAxis("Horizontal"), GetComponent<Rigidbody2D>().velocity.y);
+                //Debug.Log (GetComponent<Rigidbody2D>().velocity);
+            }
+        }
 
 	}
 
@@ -49,8 +67,11 @@ public class Player_Movement : MonoBehaviour {
 			speed = 5.00f;
 		}//this says if the player hits the floor and the dash is over, they will return to normal speed
 
-		if (theCollision.gameObject.tag == "Floor" && (Input.GetKey(KeyCode.Space))) {
+        if ((theCollision.gameObject.tag == "Floor" && GetComponent<Rigidbody2D>().velocity.y < 10) && (Input.GetKey(KeyCode.Space))) 
+        {
+            //jumpTimer = JUMPCOOLDOWN;
 			GetComponent<Rigidbody2D>().AddForce(Vector2.up * 450);
 		}
+
 	}
 }
