818ca59a294b57f5d93e99f18510d856ab65f925
unknown <mlviscon@portal.lab.huskygames.com>
2015-10-02 15:56:23 -0400

    changed network architecture

--- Battle Royal/Assets/Scripts/Player_Movement.cs
+++ Battle Royal/Assets/Scripts/Player_Movement.cs
@@ -1,23 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class Player_Movement : MonoBehaviour {
+public class Player_Movement : NetworkBehaviour
+{
 
-	public float speed = 5.00f;
-	float buttonTimer = 0f;
-	float DASHTIME = 0f;
-	float dashCooldownTimer = 0.0f;
-	public float DASHCOOLDOWN = 3.0f;
+    public float speed = 5.00f;
+    float buttonTimer = 0f;
+    float DASHTIME = 0f;
+    float dashCooldownTimer = 0.0f;
+    public float DASHCOOLDOWN = 3.0f;
     public float JUMPCOOLDOWN = 0.5f;
-    float jumpTimer = 0.0f; 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (!GetComponent<NetworkView>().isMine)
+    float jumpTimer = 0.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<NetworkIdentity>().isLocalPlayer)   //need to find out if I am controlling my own player
         {
             if (buttonTimer > 0)
             {
@@ -59,19 +63,21 @@ public class Player_Movement : MonoBehaviour {
             }
         }
 
-	}
+    }
 
-	void OnCollisionStay2D(Collision2D theCollision){
-        
-		if (theCollision.gameObject.tag == "Floor" && buttonTimer == 0) {
-			speed = 5.00f;
-		}//this says if the player hits the floor and the dash is over, they will return to normal speed
+    void OnCollisionStay2D(Collision2D theCollision)
+    {
 
-        if ((theCollision.gameObject.tag == "Floor" && GetComponent<Rigidbody2D>().velocity.y < 10) && (Input.GetKey(KeyCode.Space))) 
+        if (theCollision.gameObject.tag == "Floor" && buttonTimer == 0)
+        {
+            speed = 5.00f;
+        }//this says if the player hits the floor and the dash is over, they will return to normal speed
+
+        if ((theCollision.gameObject.tag == "Floor" && GetComponent<Rigidbody2D>().velocity.y < 10) && (Input.GetKey(KeyCode.Space)))
         {
             //jumpTimer = JUMPCOOLDOWN;
-			GetComponent<Rigidbody2D>().AddForce(Vector2.up * 450);
-		}
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * 450);
+        }
 
-	}
+    }
 }
