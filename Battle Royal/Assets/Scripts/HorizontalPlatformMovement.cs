ca92a446a34d009e04772d7d7710b53f55ffab37
unknown <derricks@Minecraft.lab.huskygames.com>
2015-10-02 16:05:20 -0400

    Created scripts for platform movements as well as a new scene

---
+++ Battle Royal/Assets/Scripts/HorizontalPlatformMovement.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalPlatformMovement : MonoBehaviour {
+
+    int right = 0;
+    int left = 0;
+    public bool moveRightFirst = true;
+    float horizontalPlatformSpeed = 0;
+    public float distanceToMove = 5;
+
+
+// Use this for initialization
+void Start()
+    {
+        if (moveRightFirst == false)
+        {
+            right = (int)((distanceToMove - 2.5) / .05) + 100;
+        }
+    }
+    
+// Update is called once per frame
+void Update()
+    {
+       
+        Vector3 up = new Vector3(0, (float)0.05, 0);
+        Vector3 down = new Vector3(0, (float)-0.05, 0);
+        
+        if (right < 50)
+        {
+            transform.Translate(horizontalPlatformSpeed, 0, 0);
+            right++;
+            horizontalPlatformSpeed += (float).001;
+            
+        }
+        else if (right < ((distanceToMove-2.5)/.05)+50)
+        {
+            transform.Translate(horizontalPlatformSpeed, 0, 0);
+            right++;
+            
+        }
+        else if (right < ((distanceToMove - 2.5) / .05) + 100)
+        {
+            transform.Translate(horizontalPlatformSpeed, 0, 0);
+            right++;
+            horizontalPlatformSpeed += (float)-.001;
+        }
+        else if (left < 50)
+        {
+            transform.Translate(horizontalPlatformSpeed, 0, 0);
+            left++;
+            horizontalPlatformSpeed += (float)-.001;
+        }
+        else if (left < ((distanceToMove-2.5)/.05)+50)
+        {
+            transform.Translate(horizontalPlatformSpeed, 0, 0);
+            left++;
+        }
+        else if (left < ((distanceToMove - 2.5) / .05) + 100)
+        {
+            transform.Translate(horizontalPlatformSpeed, 0, 0);
+            left++;
+            horizontalPlatformSpeed += (float).001;
+        }
+        else
+        {
+            left = 0;
+            right = 0;
+        }
+
+    }
+}
---
+++ Battle Royal/Assets/Scripts/VerticalPlatformMovement.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerticalPlatformMovement : MonoBehaviour {
+    int top = 0;
+    int bot = 0;
+    float platformMovementSpeed = 0;
+    public bool moveUpFirst = true;
+    public float distanceToMove = 5;
+	// Use this for initialization
+	void Start () {
+        if(moveUpFirst==false)
+        top = (int)((distanceToMove - 2.5) / .05) + 100;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 up = new Vector3(0, (float)0.05, 0);
+        Vector3 down = new Vector3(0, (float)-0.05, 0);
+       // Debug.Log(platformMovementSpeed);      
+        if(top < 50)
+        {
+            transform.Translate(0, platformMovementSpeed, 0);
+            top++;
+            platformMovementSpeed += (float).001;
+        }
+        else if (top< (int)((distanceToMove - 2.5) / .05) + 50)
+        {
+            transform.Translate(0, platformMovementSpeed, 0);
+            top++;
+        }
+        else if(top< (int)((distanceToMove - 2.5) / .05) + 100)
+        {
+            transform.Translate(0, platformMovementSpeed, 0);
+            top++;
+            platformMovementSpeed += (float)-.001;
+        }
+        else if (bot < 50)
+        {
+            transform.Translate(0, platformMovementSpeed, 0);
+            bot++;
+            platformMovementSpeed += (float)-.001;
+        }
+        else if(bot < (int)((distanceToMove - 2.5) / .05) + 50)
+        {
+            transform.Translate(0, platformMovementSpeed, 0);
+            bot++;
+        }
+        else if (bot < (int)((distanceToMove - 2.5) / .05) + 100)
+        {
+            transform.Translate(0, platformMovementSpeed, 0);
+            bot++;
+            platformMovementSpeed += (float).001;
+        }
+        else
+        {
+            bot = 0;
+            top = 0;
+        }
+	
+	}
+}
