43e44675f8cb25437b9f39b26311021825e93b04
unknown <mlviscon@portal.lab.huskygames.com>
2015-10-12 15:54:55 -0400

    Player flip animation on server and client

---
+++ Battle Royal/Assets/Scripts/Player_Server.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_Server : NetworkBehaviour {
+
+	public Player_Movement player;
+	
+	// Use this for initialization
+	void Start () {
+		if (NetworkClient.active) {
+			player.EventFlip += flip;
+		}
+	}
+
+	// Update is called once per frame
+	[ServerCallback]
+	void Update () {
+		
+	}
+
+	public void flip(){
+		transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+	}
+}
