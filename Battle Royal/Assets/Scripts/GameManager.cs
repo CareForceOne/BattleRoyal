91686b4bc6d05dc6dc42dcb35384f3bb5b8e75ab
unknown <mlviscon@portal.lab.huskygames.com>
2015-11-02 15:15:46 -0500

    fixed player spawn bug

--- Battle Royal/Assets/Scripts/GameManager.cs
+++ Battle Royal/Assets/Scripts/GameManager.cs
@@ -5,10 +5,13 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 
 	public GameObject playerPrefab;
+	public GameObject manager;
 
 	public void playerWasKilled(Player player){
 		NetworkConnection conn = player.connectionToClient;
-		GameObject newPlayer = Instantiate<GameObject> (playerPrefab);
+		Transform spawnPoint = manager.GetComponent<NetworkManager> ().GetStartPosition ();
+		//GameObject newPlayer = Instantiate<GameObject> (playerPrefab);
+		GameObject newPlayer = (GameObject) Instantiate (playerPrefab, spawnPoint.position, spawnPoint.rotation);
 		Destroy (player.gameObject);
 
 		NetworkServer.ReplacePlayerForConnection (conn, newPlayer, 0);
