5630a34f4cd87c7edb0704d3e4cf3ed6c959b56a
unknown <arfanten@titanfall.lab.huskygames.com>
2015-12-01 23:10:46 -0500

    Fixed bug with multiple network managers

---
+++ Battle Royal/Assets/Scripts/DeleteExtraManagers.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteExtraManagers : MonoBehaviour {
+
+	void Awake()
+    {
+        GameObject manager = GameObject.Find("NetworkManager");
+
+        if(manager != null)
+        {
+            Destroy(manager.gameObject);
+        }
+    }
+}
